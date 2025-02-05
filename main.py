65f3c3a14a4f2d72c81f050c1f744e3c5f4206da
qubistt <mint@mint.com>
2025-02-05 08:16:33 +0000

    added defns

--- main.py
+++ main.py
@@ -7,16 +7,33 @@ or split into powers of 2
 base 2 to base 10
 ??
 '''
-
+'''
+todo
+1. make it output int instead of list
+2. make it run inf until user asks it to stop
+3. add bnum to num functionality
+'''
 print("welcome.")
-num = int(input("what is your num?"))
-bnum = []
-while True:
-    bnum.insert(0, num%2)
-    num = num//2
-    if num <= 0:
-        print(bnum)
-        break
-    else:
-        continue
+def num2bnum():
+    bnum = []
+    num = int(input("what is num? "))
+    while True:
+        bnum.insert(0, num%2)
+        num = num//2
+        if num <= 0:
+            print(bnum)
+            break
+        else:
+            continue
 
+def num2bnum(bnum):
+   for i in bnum
+
+while True:
+    choice = input("n2b, b2n or exit")
+    if choice.lower() == 'n2b':
+        num2bnum()
+    elif choice.lower() == 'b2n':
+        bnum2num()
+    elif choice.lower() == 'exit':
+        exit()
